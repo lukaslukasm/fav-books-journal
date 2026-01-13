f16923dbf77f19850d6b0c5e3b5698bad64937d7
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-13 22:51:44 +0100

    init Material UI and sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
+
 function App() {
-	return <></>;
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<main className='app'>
+				<h1 className='heading'>Favourite Books Journal</h1>
+				<div className='main-container'></div>
+			</main>
+		</ThemeProvider>
+	);
 }
 
 export default App;
