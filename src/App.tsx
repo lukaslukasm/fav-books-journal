8e133652b1186e05eebb36c5f48e09992ed69fc5
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-13 23:08:07 +0100

    styling tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 		<ThemeProvider theme={darkTheme}>
 			<CssBaseline />
 			<main className='app'>
-				<h1 className='heading'>Favourite Books Journal</h1>
+				<h1 className='main-heading'>Favourite Books Journal</h1>
 				<div className='main-container'></div>
 			</main>
 		</ThemeProvider>
