9f44fecaa5740043faeb0866e54430a6be9946b4
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-14 22:57:14 +0100

    remove Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import BookList from './components/BookList';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
 
-const darkTheme = createTheme({
-	palette: {
-		mode: 'dark',
-	},
-});
-
 function App() {
 	return (
-		<ThemeProvider theme={darkTheme}>
-			<Provider store={store}>
-				<CssBaseline />
-				<main className='app'>
-					<h1 className='main-heading'>Favourite Books Journal</h1>
-					<div className='main-container'>
-						<div
-							className=''
-							style={{ flexGrow: 1 }}
-						></div>
-						<BookList />
-					</div>
-				</main>
-			</Provider>
-		</ThemeProvider>
+		<Provider store={store}>
+			<main className='app'>
+				<h1 className='main-heading'>Favourite Books Journal</h1>
+				<div className='main-container'>
+					<div
+						className=''
+						style={{ flexGrow: 1 }}
+					></div>
+					<BookList />
+				</div>
+			</main>
+		</Provider>
 	);
 }
 
