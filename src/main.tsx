6191d5c0144963a43500fafb7532d9d20baca3ca
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-15 00:05:40 +0100

    [refactor] remove 3rd party css resets and normalizers

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/global.scss';
-import 'normalize.css';
 import App from './App.tsx';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
