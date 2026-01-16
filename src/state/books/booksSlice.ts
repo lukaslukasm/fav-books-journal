f6e5f3ee098cfdb85aa21d48dce865619caad23b
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-16 14:43:05 +0100

    add filter by title selector

--- src/state/books/booksSlice.ts
+++ src/state/books/booksSlice.ts
@@ -1,15 +1,33 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import { type Book } from "@/src/types/types";
 import getFakeBooks from "@/src/utils/getFakeBooks";
+import type { RootState } from "../store";
 
 const booksSlice = createSlice({
   name: "books",
-  initialState: getFakeBooks(1000),
+  initialState: {
+    items: getFakeBooks(1000),
+    titleFilterQuery: "",
+  },
   reducers: {
     addBook: (state, action: PayloadAction<Book>) => {
-      state.push(action.payload);
+      state.items.unshift(action.payload);
+    },
+    setTitleFilterQuery: (state, action: PayloadAction<string>) => {
+      state.titleFilterQuery = action.payload;
     },
   },
 });
 
+export const { addBook, setTitleFilterQuery } = booksSlice.actions;
 export default booksSlice.reducer;
+
+export const selectFilteredBooks = (state: RootState) => {
+  if (!state.books.titleFilterQuery) return state.books.items;
+
+  return state.books.items.filter((book) =>
+    book.name
+      .toLowerCase()
+      .includes(state.books.titleFilterQuery.toLowerCase()),
+  );
+};
