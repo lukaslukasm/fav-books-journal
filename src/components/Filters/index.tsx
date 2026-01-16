265207e23af96837adff68e92a6aaa6cb6a193bd
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-16 15:01:46 +0100

    add responsivity to the filters and filter by title components

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,9 +1,11 @@
 import FilterByTitle from "./FilterByTitle";
 import "./style.scss";
 
+/**
+ * Renders styled container with filtering options.
+ *
+ */
 export default function Filters() {
-  // let's imagine this would be designed to effortlessly accomodate other filters
-  // without breaking the UI
   return (
     <div className="filters">
       <FilterByTitle />
